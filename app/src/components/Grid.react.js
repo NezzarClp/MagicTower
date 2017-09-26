dbff75218d81207df9f6db4ebc8c5710522ff496
Theogry <theogry@gmail.com>
2017-09-26 15:16:06 +0000

    Created Class Grid Target for tomorrow: set up stylesheets and render a 1x1 grid with stone

---
+++ app/src/components/Grid.react.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Grid extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Hi </h1>     
+            </div>
+        );
+    }
+}
